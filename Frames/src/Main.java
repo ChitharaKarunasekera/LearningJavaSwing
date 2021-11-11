2593f379faadacb9656d8fe75ed940b44b95c18e
Chithara <chithara.2019160@iit.ac.lk>
2021-11-11 11:05:38 +0530

    started project

---
+++ Frames/src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        JFrame frame = new JFrame(); //An instance of a frame to create frame
+//        frame.setTitle("My first Frame"); //title of frame
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Quit application
+//        frame.setResizable(false); //prevent from being resized
+//        frame.setSize(420,420); //sets the size of frame
+//        frame.setVisible(true);// make table visible
+//
+//
+//        ImageIcon image = new ImageIcon("CompanyName.png");//create an image icon
+//        frame.setIconImage(image.getImage());//change icon of frame
+//        frame.getContentPane().setBackground(new Color(0,255,0));//change colour of background
+
+        //instance of MYFrame
+        MyFrame myFrame = new MyFrame();
+    }
+}
