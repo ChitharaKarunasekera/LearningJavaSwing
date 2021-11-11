5f8fd86f906efa802bfdf93dd3b58f254ed4a9b8
Chithara <chithara.2019160@iit.ac.lk>
2021-11-11 11:08:16 +0530

    use of frames

--- Frames/src/Main.java
+++ Frames/src/Main.java
@@ -4,6 +4,10 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
+        //instance of MYFrame
+        MyFrame myFrame = new MyFrame();
+
+        
 //        JFrame frame = new JFrame(); //An instance of a frame to create frame
 //        frame.setTitle("My first Frame"); //title of frame
 //        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Quit application
@@ -15,8 +19,5 @@ public class Main {
 //        ImageIcon image = new ImageIcon("CompanyName.png");//create an image icon
 //        frame.setIconImage(image.getImage());//change icon of frame
 //        frame.getContentPane().setBackground(new Color(0,255,0));//change colour of background
-
-        //instance of MYFrame
-        MyFrame myFrame = new MyFrame();
     }
 }
