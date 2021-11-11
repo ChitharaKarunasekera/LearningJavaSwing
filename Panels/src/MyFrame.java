22bd143418be26e178822e818f449f2b28cb7824
Chithara <chithara.2019160@iit.ac.lk>
2021-11-11 13:10:17 +0530

    more on panels

---
+++ Panels/src/MyFrame.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+//MyFrame is subclass and JFrame is the parent class
+public class MyFrame extends JFrame {
+
+    public MyFrame(){
+        this.setTitle("My first Frame"); //title of frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Quit application
+        this.setResizable(false); //prevent from being resized
+        this.setSize(420,420); //sets the size of frame
+        this.setVisible(true);// make table visible
+
+
+        ImageIcon image = new ImageIcon("CompanyName.png");//create an image icon
+        this.setIconImage(image.getImage());//change icon of frame
+        this.getContentPane().setBackground(new Color(0,255,0));//change colour of background
+    }
+}
