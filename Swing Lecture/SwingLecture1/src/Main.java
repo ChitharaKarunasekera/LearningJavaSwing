60498c3f659354a031b03b29476d29e25c6718d5
Chithara <chithara.2019160@iit.ac.lk>
2021-11-18 13:18:45 +0530

    example 2

--- Swing Lecture/SwingLecture1/src/Main.java
+++ Swing Lecture/SwingLecture1/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Simple JFrame Example");
-        JButton b = new JButton("Click");
-        b.setBounds(200, 100, 100, 50);
+        JButton b = new JButton("Click"); //creating a button
+        b.setBounds(200, 100, 100, 50);//configure location and size of button
 
         frame.add(b);
         frame.setSize(400, 500);
